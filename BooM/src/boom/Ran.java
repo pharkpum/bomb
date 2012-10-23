ca0d978017b7e4d934af233eb3d444521ebf3637
pharkpum Kowwannasri <sabatisa@windowslive.com>
2012-10-23 15:01:47 +0700

    add first

---
+++ BooM/src/boom/Ran.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boom;
+
+/**
+ *
+ * @author THE_1
+ */
+public class Ran {
+    public int Randombomb(){
+        int i = (int) ( Math.random()* 5 )+1;
+        return i;
+    }
+}
