44786a55d7b043d6d543b200d5cb9d35171d55df
chaowarat <chaowaratt@gmail.com>
2012-10-23 16:44:45 +0700

    add test

---
+++ BooM/BooM/test/boom/RanTest.java
@@ -0,0 +1,36 @@
+package boom;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class RanTest {
+    
+    public RanTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testRandombomb() {
+        assertEquals(1, 1);
+    }
+}
